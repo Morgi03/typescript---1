e563c7a70786e2635b73d9bc9ef33a9900829a45
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-05 12:59:08 +0200

    első .ts megírása

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 "use strict";
-let message = 'hello world';
+// Automata fordítás
+// npx tsc --watch
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _User_nev, _User_eletkor;
+class User {
+    constructor(nev, eletkor) {
+        _User_nev.set(this, void 0);
+        _User_eletkor.set(this, void 0);
+        __classPrivateFieldSet(this, _User_nev, nev, "f");
+        __classPrivateFieldSet(this, _User_eletkor, eletkor, "f");
+    }
+    kiir() {
+        console.log(__classPrivateFieldGet(this, _User_nev, "f"), __classPrivateFieldGet(this, _User_eletkor, "f"));
+    }
+}
+_User_nev = new WeakMap(), _User_eletkor = new WeakMap();
+let message = 'Hello world';
 console.log(message);
+// let ures; <- nem találja ki a tipust 
+if (message.startsWith('H')) {
+    console.log('H-val kezdődik');
+}
+else {
+    console.log();
+}
+let ertek = 5;
+// Kitalálja magától:
+// ertek = "asdf"; <-- hiba!
+function negyzet(szam) {
+    return szam * szam;
+}
+// let s : string = negyzet(3); hibát jelez
+let user1 = new User('nev', 44);
+user1.kiir();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,41 @@
-let message : string = 'hello world';
+// Automata fordítás
+// npx tsc --watch
+
+class User {
+    #nev : string;
+    #eletkor : number;
+
+    constructor(nev: string, eletkor : number){
+        this.#nev = nev;
+        this.#eletkor = eletkor;
+    }
+
+    kiir() : void {
+        console.log(this.#nev, this.#eletkor);
+    }
+
+}
+
+
+let message : string = 'Hello world';
 console.log(message);
+// let ures; <- nem találja ki a tipust 
+if(message.startsWith('H')) {
+    console.log('H-val kezdődik')
+}else{
+    console.log()
+}
+let ertek = 5;
+
+// Kitalálja magától:
+// ertek = "asdf"; <-- hiba!
+
+
+function negyzet(szam : number) : number {
+    return szam * szam;
+}
+
+// let s : string = negyzet(3); hibát jelez
+
+let  user1 = new User('nev',44);
+user1.kiir();
